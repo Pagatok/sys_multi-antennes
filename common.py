dd1b7eb2b69546296c856a47cdc6c55d2e55d31b
Raphaël Kuhn <pagatok@proton.me>
2025-10-22 16:22:36 +0200

    Premiere implementation de toute la chaine, resultats pas tres concluants

--- common.py
+++ common.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-lambda_c = 0.3
+lambda_c = 1
 
-def steering_vector(theta, M, d, lambda_c=lambda_c):
+def steering_vector(theta, M, d=1, lambda_c=lambda_c):
 
-    m = np.arange(M)  # indices des antennes 0, 1, ..., M-1
-    # phase = -1j * 2 * np.pi * d / lambda_c * m * np.sin(np.deg2rad(theta))
-    phase = -1j * np.pi * m * np.sin(np.deg2rad(theta))
+    liste_m = np.arange(M)  # indices des antennes 0, 1, ..., M-1
+    phase = -1j * np.pi * (d/lambda_c) * liste_m * np.sin(np.deg2rad(theta))
     a = np.exp(phase)
     return a
 
